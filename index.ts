8e1b3ab80ef158acf377b9785e3282a2ccbe8be3
Robert McGregor <117464563+Zylifv@users.noreply.github.com>
2026-03-16 13:10:04 +0000

    secondary

--- index.ts
+++ index.ts
@@ -12,3 +12,27 @@ app.listen(port, () => {
 app.get("/", (req, res) => {
   res.send({ data });
 });
+
+/*app.get("/", (req, res) => {
+  const singleTypePokemon = [
+    ...data.pokemon
+      .filter((d: any) => d.types[1] === "None")
+      .map((d: any) => d.name),
+  ];
+
+  res.send({ data, singleTypePokemon });
+});
+
+console.log([
+  ...data.pokemon.filter(
+    (d: any) => d.types[0] === "Grass" && d.types[1] === "Poison"
+  ),
+]);
+
+for (let i = 0; i < data.pokemon.length; i++) {
+  if (data.pokemon[i].types[1] === "None") {
+    console.log(data.pokemon[i].name);
+  }
+}*/
+
+console.log(data.pokemon[0].types); //['Grass', 'Poison']; => works
